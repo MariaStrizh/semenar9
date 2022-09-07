82111e50783bad1b2a7bd5e3a4ba4fa09b4bea99
Maria <str_maria@mail.ru>
2022-09-07 09:33:41 +0300

    Домашняя работа task64 поправила

--- task64/Program.cs
+++ task64/Program.cs
@@ -2,25 +2,13 @@
 которая выведет все натуральные числа в промежутке от N до 1.
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
-
-Console.WriteLine("Введите число M:");
-int numberM=Convert.ToInt32(Console.ReadLine());
-
 Console.WriteLine("Введите число N:");
-int numberN=Convert.ToInt32(Console.ReadLine());
-
-if (numberN>numberM)
+int number=Convert.ToInt32(Console.ReadLine());
+ PrintNumberstoN (number);
+void PrintNumberstoN (int n)
 {
-    PrintNumberstoN (numberN, numberM);
-}
-else
-{
-    Console.WriteLine("M должно быть меньше N");
-}
-
-void PrintNumberstoN (int n, int m)
-{
-if (n < m) return;
+if (n < 1) return;
 Console.Write(n + " ");
-PrintNumberstoN (n-1,m); // чтобы выводилось в другом порядке надо поменять местами последние 2 строчки
+PrintNumberstoN (n-1); // чтобы выводилось в другом порядке надо поменять местами последние 2 строчки
+
 }
