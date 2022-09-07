6ac7c4274940558bf0ec8895eba2f0a473bebfc7
Maria <str_maria@mail.ru>
2022-09-07 09:29:20 +0300

    Домашняя работа task64

---
+++ task64/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от N до 1.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+Console.WriteLine("Введите число M:");
+int numberM=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N:");
+int numberN=Convert.ToInt32(Console.ReadLine());
+
+if (numberN>numberM)
+{
+    PrintNumberstoN (numberN, numberM);
+}
+else
+{
+    Console.WriteLine("M должно быть меньше N");
+}
+
+void PrintNumberstoN (int n, int m)
+{
+if (n < m) return;
+Console.Write(n + " ");
+PrintNumberstoN (n-1,m); // чтобы выводилось в другом порядке надо поменять местами последние 2 строчки
+}
